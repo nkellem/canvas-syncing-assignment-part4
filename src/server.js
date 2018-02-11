121f36710d5bd9893fe2998db15a3a035a704f24
nkellem <noahkellem@gmail.com>
2018-02-11 15:38:05 -0500

    part 4 completed

---
+++ src/server.js
@@ -0,0 +1,54 @@
+const http = require('http');
+const fs = require('fs');
+const socketio = require('socket.io');
+
+const PORT = process.env.PORT || process.env.NODE_PORT || 3000;
+
+const index = fs.readFileSync(`${__dirname}/../client/index.html`);
+
+const onRequest = (request, response) => {
+  response.writeHead(200, { 'Content-Type': 'text/html' });
+  response.write(index);
+  response.end();
+};
+
+const app = http.createServer(onRequest).listen(PORT);
+
+console.log(`Listening on localhost:${PORT}`);
+
+// pass in the http server into socketio and grab the websocket sever as io
+const io = socketio(app);
+
+const onJoined = (sock) => {
+  const socket = sock;
+
+  socket.on('join', () => {
+    socket.join('room1');
+  });
+};
+
+const onDraw = (sock) => {
+  const socket = sock;
+
+  socket.on('draw', (data) => {
+    io.sockets.in('room1').emit('outStream', data);
+  });
+};
+
+const onDisconnect = (sock) => {
+  const socket = sock;
+
+  socket.on('disconnect', () => {
+    socket.leave('room1');
+  });
+};
+
+io.sockets.on('connection', (socket) => {
+  console.log('started');
+
+  onJoined(socket);
+  onDraw(socket);
+  onDisconnect(socket);
+});
+
+console.log('Websocket server started');
